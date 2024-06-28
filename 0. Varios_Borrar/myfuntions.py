032ef5454fe56937272fe552fa90f1a0c4e15b2f
Jacqueline Guzman R. <jacqueline.guzman.r@gmail.com>
2024-06-28 15:33:19 -0500

    Clean test files

--- 0. Varios_Borrar/myfuntions.py
+++
@@ -1,8 +0,0 @@
-def string_to_num_string(string):
-    num_string=0
-    for i in range(len(string)):
-        num=int(ord(string[i]))
-        #print(num)
-        num_string=num_string+num
-    #print('Este es el valor de la cadena: '+cadena+' '+str(num_cadena))
-    return num_string
